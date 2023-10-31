726ccd3a044321291035a7c178c6a2cf9bc71d7e
xiaokai <xiaokai@kk.com>
2023-10-31 20:02:06 +0700

    配置

--- mysql_init/mysql_init.sql
+++ mysql_init/mysql_init.sql
@@ -17,10 +17,10 @@ create table sehuatang.sht_data (
     tid int null comment '网站页面标识',
     fid int null comment '板块标识',
     file_size varchar(150) null comment '文件大小',
-    url varchar(200) null comment '色花堂地址',
+    url varchar(200) null comment '色花堂地址'
 );
 create table sehuatang.sht_images (
     id int auto_increment comment 'id' primary key,
     sht_data_id int null comment '关联数据id',
-    image_url varchar(200) null comment '图片地址',
+    image_url varchar(200) null comment '图片地址'
 );
